b1437c5e02e122c2ed9ec5aea73d646b5ea95a80
Kaedurr <katedbee@gmail.com>
2022-03-13 14:49:34 -0400

    implement sticky return to top button
    Implement sticky "Return to top" button properly, using JS, replacing existing html # linking method

--- js/main.js
+++ js/main.js
@@ -4,3 +4,23 @@ jQuery(function($) {'use strict',
 	new WOW().init();
 	
 });
+
+//Get the stickyBtn:
+mybutton = document.getElementById("stickyBtn");
+
+// When the user scrolls down 20px from the top of the document, show the stickyBtn
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the stickyBtn, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
